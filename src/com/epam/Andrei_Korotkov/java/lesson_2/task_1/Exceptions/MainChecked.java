94b1fdb856f0413cea2eb45fc44918cd3f1ad42e
Andrei Korotkov <andrei_korotkov@epam.com>
2019-08-07 19:39:59 +0400

    added exceptions

---
+++ src/com/epam/Andrei_Korotkov/java/lesson_2/task_1/Exceptions/MainChecked.java
@@ -0,0 +1,10 @@
+package com.epam.Andrei_Korotkov.java.lesson_2.task_1.Exceptions;
+
+/**
+ * created by Andrei_Korotkov 8/7/2019
+ */
+public class MainChecked extends Exception {
+    public MainChecked(String message) {
+        super(message);
+    }
+}
---
+++ src/com/epam/Andrei_Korotkov/java/lesson_2/task_1/Exceptions/NullEngineException.java
@@ -0,0 +1,12 @@
+package com.epam.Andrei_Korotkov.java.lesson_2.task_1.Exceptions;
+
+/**
+ * created by Andrei_Korotkov 8/7/2019
+ */
+final public class NullEngineException extends MainChecked {
+
+    public NullEngineException(String message) {
+        super (message);
+    }
+
+}
